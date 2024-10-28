b5ff167f60831876b9282c79b234faa0e22e95ab
Linh Le <linh.le@harborglobal.com>
2024-10-27 20:39:51 -0400

    Playwright

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 export PLAYWRIGHT_BROWSERS_PATH=0
 
 # Force reinstallation of the exact Chromium 1140 version
-npx playwright install chromium
+npx playwright install chromium@114.0.5735.90
 
 # Verify that the correct version is installed
 ls -alh /root/.cache/ms-playwright/
